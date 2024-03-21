c013878be67cf399470ee82feeb02da3660035c9
Daniel Brian <db9755949@gmail.com>
2024-03-21 23:46:28 +0300

    Add: Splash and Onboarding screen

---
+++ app/src/main/java/daniel/brian/news_app/presentation/Dimens.kt
@@ -0,0 +1,12 @@
+package daniel.brian.news_app.presentation
+
+import androidx.compose.ui.unit.dp
+
+object Dimens {
+    val mediumPadding1 = 24.dp
+    val mediumPadding2 = 30.dp
+
+    val indicatorSize = 14.dp
+
+    val pageIndicatorSize = 52.dp
+}
